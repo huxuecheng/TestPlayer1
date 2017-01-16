5299579334ad419ceb88486811bffc9a5e931f04
247249879@qq.com <247249879@qq.com>
2017-01-16 10:31:50 +0800

    Initial commit

---
+++ app/src/main/java/com/atguigu/testplayer/view/TitleBarView.java
@@ -0,0 +1,21 @@
+package com.atguigu.testplayer.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+
+/**
+ * Created by 呼学成 on 16/01/2017.
+ */
+
+public class TitleBarView extends LinearLayout implements View.OnClickListener {
+    public TitleBarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
